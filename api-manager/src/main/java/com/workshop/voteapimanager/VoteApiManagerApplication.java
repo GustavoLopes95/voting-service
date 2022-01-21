bad49d72451cdec53d484b4c33bd525403e39ea9
Gustavo Lopes <lgalopes@motoristapx.com.br>
2022-01-21 00:58:52 -0300

    Base structure project

---
+++ api-manager/src/main/java/com/workshop/voteapimanager/VoteApiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.voteapimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteApiManagerApplication.class, args);
+	}
+
+}
