79fb6c329712feab5f61f72302f59db8d183b399
Gustavo Lopes <lgalopes@motoristapx.com.br>
2022-01-31 19:16:45 -0300

    Retrive db-cache info

--- api/src/main/java/com/workshop/vote/infra/data/mapper/TopicRedisMapper.java
+++ api/src/main/java/com/workshop/vote/infra/data/mapper/TopicRedisMapper.java
@@ -1,11 +1,39 @@
 package com.workshop.vote.infra.data.mapper;
 
 import com.workshop.vote.domain.entities.OpenedTopic;
+import com.workshop.vote.domain.factories.OpenedTopicFactory;
 import com.workshop.vote.infra.data.model.TopicRedisModel;
+import org.javatuples.Ennead;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Instant;
 
 public class TopicRedisMapper {
 
-    public static TopicRedisModel toRepository(OpenedTopic topic) {
+    private OpenedTopicFactory openedTopicFactory;
+
+    @Autowired
+    public TopicRedisMapper(OpenedTopicFactory openedTopicFactory) {
+        this.openedTopicFactory = openedTopicFactory;
+    }
+
+    public TopicRedisModel toRepository(OpenedTopic topic) {
         return new TopicRedisModel(topic.getId(), topic.getName(), topic.getStatus());
+    }
+
+    public OpenedTopic toDomain(TopicRedisModel model) {
+        return this.openedTopicFactory.create(
+                Ennead.with(
+                        model.getId(),
+                        model.getName(),
+                        60.0,
+                        "username",
+                        Instant.now(),
+                        "usernameTwo",
+                        Instant.now(),
+                        "api-vote",
+                        1
+                )
+        );
     }
 }
