dfcccd06a2c35822dba26b1899f95489b2c06303
Gustavo Lopes <lgalopes@motoristapx.com.br>
2022-02-02 01:24:31 -0300

    [FEATURE] - create ComputedExpiredTopicSchedulerUseCase and call it in Scheduler

--- api/src/main/java/com/workshop/vote/application/scheduler/ComputeExpiredTopicScheduler.java
+++ api/src/main/java/com/workshop/vote/application/scheduler/ComputeExpiredTopicScheduler.java
@@ -1,18 +1,30 @@
 package com.workshop.vote.application.scheduler;
 
+import com.workshop.vote.application.useCase.ComputeExpiredTopicUseCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Component
 public class ComputeExpiredTopicScheduler {
 
     private static final Logger log = LoggerFactory.getLogger(ComputeExpiredTopicScheduler.class);
+    private ComputeExpiredTopicUseCase useCase;
+
+    @Autowired
+    public ComputeExpiredTopicScheduler(ComputeExpiredTopicUseCase useCase) {
+        this.useCase = useCase;
+    }
 
     @Scheduled(fixedDelay = 5000)
     public void run() {
-        log.info("Hello world");
+        log.info("Run ComputeExpiredTopicScheduler at ".concat(LocalDateTime.now(ZoneOffset.UTC).toString()));
+        this.useCase.execute();
     }
 
 }
--- api/src/main/java/com/workshop/vote/application/useCase/ComputeExpiredTopicUseCase.java
+++ api/src/main/java/com/workshop/vote/application/useCase/ComputeExpiredTopicUseCase.java
@@ -1,5 +1,7 @@
 package com.workshop.vote.application.useCase;
 
+import com.workshop.vote.domain.entities.OpenedTopic;
+import com.workshop.vote.domain.interfaces.ITopicRepository;
 import com.workshop.vote.domain.interfaces.ITopicSchedulerRepository;
 import com.workshop.vote.infra.crossCutting.messages.notifications.NotificationHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +10,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class ComputeExpiredTopicUseCase extends BaseUseCase<Void> {
 
-    private ITopicSchedulerRepository repository;
+    private ITopicRepository repository;
+    private ITopicSchedulerRepository schedulerRepository;
 
     @Autowired
-    public ComputeExpiredTopicUseCase(NotificationHandler notificationHandler, ITopicSchedulerRepository repository) {
+    public ComputeExpiredTopicUseCase(NotificationHandler notificationHandler, ITopicRepository repository, ITopicSchedulerRepository schedulerRepository) {
         super(notificationHandler);
         this.repository = repository;
+        this.schedulerRepository = schedulerRepository;
     }
 
     @Override
     public Void execute() {
+        var openedTopics = this.schedulerRepository.findAll();
+        openedTopics.forEach(topic -> {
+            if(topic.isExpired()) {
+                this.compute(topic);
+                this.removeTopicFromScheduler(topic);
+            }
+        });
+
         return null;
+    }
+
+    private void compute(OpenedTopic topic) {
+        var votes = this.repository.countVoteByTopicId(topic);
+        var closedTopic = topic.close(votes);
+        this.repository.update(closedTopic);
+    }
+
+    private void removeTopicFromScheduler(OpenedTopic topic) {
+        this.schedulerRepository.remove(topic.getHash());
     }
 }
